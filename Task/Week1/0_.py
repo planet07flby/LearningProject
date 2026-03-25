a5de08d5b894816f7fbe19af27d03876c9e94334
Administrator <3495213250@qq.com>
2026-03-26 02:54:07 +0800

    0

--- Task/Week1/0_.py
+++ Task/Week1/0_.py
@@ -34,15 +34,26 @@ vol = 100  # 股
 
 def run_portfolio_strategy(time_begin, time_end, portfolio=sample, initial_capital=200000):
     import matplotlib.pyplot as plt
+    # 中文报错解决方案
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
     t = pd.date_range(start=time_begin, end=time_end, freq='B', normalize=True)
 
-    # positions = {cod: pd.DataFrame([],index=t,columns=['date','price','vol']) for cod in portfolio}  # 每个个股动态持仓记录
     cum_return = pd.Series(np.zeros(len(t)), index=t)  # 累计收益率(%)数组
     vol_sum = {cod: 0 for cod in portfolio}  # 个股动态持仓数量(股)
     cost = {cod: 0.00 for cod in portfolio}  # 个股动态持仓成本(元)
+    stop = 0
 
-    for current_date in t:
+    for i,current_date in enumerate(t):
         delta = 0
+        if stop != 0:
+            stop -= 1
+        if stop == 0:
+            if cum_return.iloc[i-1] < -10:
+                print(f" 在 {current_date.date()} 触发止损, 停止交易5天")
+                stop = 5
+
         for cod in portfolio:
             df_stock = dict_stock.get(cod)
             try:
@@ -52,24 +63,31 @@ def run_portfolio_strategy(time_begin, time_end, portfolio=sample, initial_capit
 
             cross_val = int(row['cross']) if not pd.isna(row['cross']) else 0
             price = float(row['close'])
-            if cross_val == 1:  # 买入
+            if cross_val == 1 and stop == 0:  # 买入
                 vol_sum[cod] += vol
                 cost[cod] = (cost[cod]*(vol_sum[cod]-vol) + price*vol)/vol_sum[cod]   # 更新持仓成本
-                print(f" 在 {current_date.date()} 买入 {cod}, 价格 {price}")
-            elif cross_val == -1:  # 卖出
+                print(f" 在 {current_date.date()} 买入 {cod}, 价格 {price}元")
+            elif cross_val == -1 and stop == 0:  # 卖出
                 if  vol_sum[cod] >= vol:
                     vol_sum[cod] -= vol
                     cost[cod] = (cost[cod]*(vol_sum[cod]+vol) - price*vol)/vol_sum[cod] if vol_sum[cod] > 0 else 0  # 更新持仓成本
-                    print(f" 在 {current_date.date()} 卖出 {cod}, 价格 {price}")
+                    print(f" 在 {current_date.date()} 卖出 {cod}, 价格 {price}元")
                 else:
                     pass
 
             delta += vol_sum[cod]*(price-cost[cod])
+
         cum_ret = delta/initial_capital*100
         cum_return[current_date] = cum_ret
 
+
+
+    all_capital = initial_capital * (1 + cum_return/100)
+    max_drawdown = (all_capital / all_capital.cummax()).min()
+
     plt.plot(cum_return.index, cum_return.values)
     plt.title('Cumulative Return of Portfolio')
+    plt.annotate(f'最大回撤: {max_drawdown:.2f}', xy=(0.5, 0.9), xycoords='axes fraction', fontsize=12, ha='center')
     plt.xlabel('Date')
     plt.ylabel('Cumulative Return(%)')
     plt.grid()
